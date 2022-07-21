8eb029122386b79bceb89f84dd8ac1c4b674b5b0
Jonatas Afonso <jonatas_afonso@hotmail.com>
2022-07-21 20:15:00 +0100

    Exemplo ADO

---
+++ AcessoADados/Aula03/ExemploADO/ExemploADO/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using ExemploADO.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploADO
+{
+    public interface IRepositorio
+    {
+        List<Pessoa> ObterTodos();
+        Pessoa ObterPorId(Guid id);
+        void Criar(Pessoa pessoa);
+        void Atualizar(Guid id, Pessoa pessoa);
+        void Apagar(Guid id);
+    }
+}
