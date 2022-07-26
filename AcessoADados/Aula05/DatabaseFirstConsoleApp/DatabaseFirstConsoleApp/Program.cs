508cb77be3f82a6f40bd2b1163d694d116fb626a
Jonatas Afonso <jonatas_afonso@hotmail.com>
2022-07-26 20:46:00 +0100

    Final repositorio Databnase first

--- AcessoADados/Aula05/DatabaseFirstConsoleApp/DatabaseFirstConsoleApp/Program.cs
+++ AcessoADados/Aula05/DatabaseFirstConsoleApp/DatabaseFirstConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using DatabaseFirstConsoleApp.Repositorio;
+﻿using DatabaseFirstConsoleApp.Modelo;
+using DatabaseFirstConsoleApp.Repositorio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,8 +23,13 @@ namespace DatabaseFirstConsoleApp
             var pessoaTeste = repo.ObterPorId(new Guid("85b5bf28-6685-4e7e-a627-8a7509d34e54"));
             Console.WriteLine($"A Pessoa Escolhida se chama: {pessoaTeste.Nome}");
 
-          
 
+            var pessoa = new Pessoa { Id = Guid.NewGuid(), Nome = "Dona Maria", DataNascimento = new DateTime(1965, 01, 01) };
+            repo.CriarNovo(pessoa);
+
+            repo.AtualizarNome(new Guid("6295AFF3-E222-44CE-84C9-4FD0693CDA1C"), "João Maria");
+
+            repo.Apagar(new Guid("6295AFF3-E222-44CE-84C9-4FD0693CDA1C"));
 
             Console.ReadLine();
         }
