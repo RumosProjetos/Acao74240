39764947d623e71dd87d2b5703d43b1055699367
J├┤natas Afonso <jonatas_afonso@hotmail.com>
2022-09-12 21:41:05 +0100

    Uso repositorio

---
+++ AcessoADados/AulaExtra/ExemploSQLDB/ExemploSQLDB/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploSQLDB
+{
+    public record Product
+    (
+        string id,
+        string category,
+        string name,
+        int quantity,
+        bool sale
+    );
+}
