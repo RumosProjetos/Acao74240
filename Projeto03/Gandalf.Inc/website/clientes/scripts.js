2f749649a57678fca796297bc528a12bb7ee94fc
J├┤natas Afonso <jonatas_afonso@hotmail.com>
2022-09-29 22:06:26 +0100

    Adicionado scripts.js

---
+++ Projeto03/Gandalf.Inc/website/clientes/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#tableClientes').DataTable({
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json',
+        },
+    });
+});
