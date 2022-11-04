47c9e29ee3b8e36ef9b44e008098d4b35d9e0d30
J├┤natas Afonso <jonatas_afonso@hotmail.com>
2022-11-04 19:29:18 +0000

    Adicionando bootstrap Google Graph

---
+++ Projeto04/Gandalf.Inc/Projeto.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
